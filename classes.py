398656bef0908c9dda50b05b1752ff124e5dd5eb
Ashish Sawant <sawant.ashish013@gmail.com>
2025-11-19 19:48:53 +0530

    Worked on classes.py

--- classes.py
+++ classes.py
@@ -1,29 +1,4 @@
 classes = ["Barbarian","Bard","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"] #Base Classes 
-class_traits = {
-    "Barbarian":{
-        "Description" : "Barbarians are powered by primal forces of the multiverse that manifest as a Rage. More than a mere emotion and not limited to anger, this Rage is an incarnation of a predator's ferocity, a storm's fury, and a sea's turmoil.",
-        "Primary Ability" : "Strength",
-        "Hitpoint Die" : "D12",
-        "Saving Throw Proficiencies" : ["Strength","Constitution"],
-        "Skill Proficiencies" : ["Animal Handling", "Athletics", "Intimidation", "Nature", "Perception", "Survival"],
-        "Weapon Proficiencies" : ["Simple", "Martial"],
-        "Armor Training" : ["Light", "Medium", "Shields"],
-        "Tool Proficiencies" : "None",
-        "Starting Equipment" : [["Greataxe","Handaxe","Handaxe","Handaxe","Handaxe","Explorer's Pack", "GP 15"],["GP 75"]],
-        "Class Features" : {}
-    },
-
-    "Bard":{
-        "Primary Ability" : "Charisma",
-        "Hitpoint Die" : "D8",
-        "Saving Throw Proficiencies" : ["Strength","Constitution"],
-        "Skill Proficiencies" : ["Animal Handling", "Athletics", "Intimidation", "Nature", "Perception", "Survival"],
-        "Weapon Proficiencies" : ["Simple", "Martial"],
-        "Armor Training" : ["Light", "Medium", "Shields"],
-        "Starting Equipment" : [["Greataxe","Handaxe","Handaxe","Handaxe","Handaxe","Explorer's Pack", "GP 15"],["GP 75"]]
-    }
-
-}
 
 class_features_table = {
     "Barbarian" : {
@@ -50,3 +25,34 @@ class_features_table = {
 
     }
 }
+
+class_traits = {
+    "Barbarian":{
+        "Description" : "Barbarians are powered by primal forces of the multiverse that manifest as a Rage. More than a mere emotion and not limited to anger, this Rage is an incarnation of a predator's ferocity, a storm's fury, and a sea's turmoil.",
+        "Primary Ability" : "Strength",
+        "Hitpoint Die" : "D12",
+        "Saving Throw Proficiencies" : ["Strength","Constitution"],
+        "Skill Proficiencies" : ["Animal Handling", "Athletics", "Intimidation", "Nature", "Perception", "Survival"],
+        "Weapon Proficiencies" : ["Simple", "Martial"],
+        "Armor Training" : ["Light", "Medium", "Shields"],
+        "Tool Proficiencies" : "None",
+        "Starting Equipment" : [["Greataxe","Handaxe","Handaxe","Handaxe","Handaxe","Explorer's Pack", "GP 15"],["GP 75"]],
+        "Class Features" : class_features_table["Barbarian"],
+        "Subclasses" : {"Path of Berserker":"",
+                        "Path of Wild Heart":"",
+                        "Path of the World Tree":"",
+                        "Path of Zealot":""}
+    },
+
+    "Bard":{
+        "Primary Ability" : "Charisma",
+        "Hitpoint Die" : "D8",
+        "Saving Throw Proficiencies" : ["Strength","Constitution"],
+        "Skill Proficiencies" : ["Animal Handling", "Athletics", "Intimidation", "Nature", "Perception", "Survival"],
+        "Weapon Proficiencies" : ["Simple", "Martial"],
+        "Armor Training" : ["Light", "Medium", "Shields"],
+        "Starting Equipment" : [["Greataxe","Handaxe","Handaxe","Handaxe","Handaxe","Explorer's Pack", "GP 15"],["GP 75"]]
+    }
+
+}
+
